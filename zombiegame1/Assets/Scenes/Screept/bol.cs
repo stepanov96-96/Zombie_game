64ae9c5dc85ff9f0a1604a95bd41394639eede3a
stepanov96-96 <55653219+stepanov96-96@users.noreply.github.com>
2019-10-24 21:44:37 +0700

    добавил скрипты и спрайты

---
+++ zombiegame1/Assets/Scenes/Screept/bol.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bol : MonoBehaviour
+{
+    public float speed = 1.5f;
+   
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);  
+    }
+}
---
+++ zombiegame1/Assets/Scenes/Screept/pg.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class pg : MonoBehaviour
+{
+    public float spead = 20f;
+    private Rigidbody2D rb;
+    public float speed =10;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+   
+    void Update()
+    {
+        float moveY = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + Vector2.up *moveY * speed * Time.deltaTime);
+        
+    }
+}
